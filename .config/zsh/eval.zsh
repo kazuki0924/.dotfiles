1cf296c63b088874744865b979523f2b8bf1c7d5
Kazuki Matsuzaki <kazuki0924@gmail.com>
2021-05-06 00:44:08 +0900

    Add: iterm2

--- .config/zsh/eval.zsh
+++ .config/zsh/eval.zsh
@@ -5,3 +5,6 @@ export PATH="$PATH:$GOPATH/bin"
 
 # dircolors / gdircolors
 eval "$(gdircolors $HOME/Miscellaneous/dircolors-solarized/dircolors.256dark)"
+
+# iterm2
+# test -e $HOME/.iterm2_shell_integration.zsh && source $HOME/.iterm2_shell_integration.zsh
