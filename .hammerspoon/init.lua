902c2ce359f4fd1afc5d198b276679e2b1abe1f9
Kazuki M <kazuki0924@gmail.com>
2021-06-07 13:38:41 +0900

    Add: yabai

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,28 @@
 alacritty_move_focus = require "misc/alacritty_move_focus"
 
 hs.hotkey.bind({"cmd", "ctrl"}, "I", alacritty_move_focus)
+
+-- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+--   hs.alert.show("Hello World!")
+-- end)
+
+-- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+--   hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+-- end)
+
+-- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
+--   local win = hs.window.focusedWindow()
+--   local f = win:frame()
+
+--   f.x = f.x - 10
+--   win:setFrame(f)
+-- end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "T", function()
+  local laptopScreen = "MBP"
+  local windowLayout = {
+      {"Google Chrome",  nil,          laptopScreen, hs.layout.left30,    nil, nil},
+      {"Alacritty",    nil,          laptopScreen, hs.layout.right70,   nil, nil},
+  }
+  hs.layout.apply(windowLayout)
+end)
--- .zshrc
+++ .zshrc
@@ -1,10 +1,1 @@
-__source() {
-  for file in ~/.config/"$1"/*.zsh; do
-    source $file
-  done
-}
-
-__source "zsh/before"
-__source "zsh"
-__source "zsh/after"
-unset -f __source
+source "$HOME/.zshfiles/main.zsh"
